a3f2e56d03268018ef841180731bb1d20132ffce
Ethan <144255690+Ethan-Sarricolea@users.noreply.github.com>
2024-02-26 10:44:07 -0600

    Update Sucesiones.py

--- Sucesiones.py
+++ Sucesiones.py
@@ -1,5 +1,5 @@
 """
-Ejercicios de sucesion del 10 al 14 (se me paso la 13)
+Ejercicios de sucesion del 10 al 14
 Autor: Ethan Yahel Sarricolea Cortés
 """
 
@@ -19,7 +19,12 @@ def sus14():
     for i in range(1,(int(input("Coloca el largo: ")))+1):
         print(f"{i**2}/{3**i}")
 
+def sus15():
+    for i in range(1,(int(input("Coloca el largo: ")))+1):
+        print("0" if i==1 else f"{(i-1)*((-1)**(i+1))}/{i}")
+
 sus10()
 sus11()
 sus12()
 sus14()
+sus15()
